2e23a24fc725eaf023f8c0824ed1fa39f4f7da10
Henry Zhu <hi@henryzoo.com>
2018-08-09 09:42:54 -0400

    add individual proposals from core-js v2

---
+++ packages/babel-polyfill/src/math-extensions.js
@@ -0,0 +1,12 @@
+import "core-js/fn/math/clamp";
+import "core-js/fn/math/deg-per-rad";
+import "core-js/fn/math/degrees";
+import "core-js/fn/math/fscale";
+import "core-js/fn/math/iaddh";
+import "core-js/fn/math/isubh";
+import "core-js/fn/math/imulh";
+import "core-js/fn/math/rad-per-deg";
+import "core-js/fn/math/radians";
+import "core-js/fn/math/scale";
+import "core-js/fn/math/umulh";
+import "core-js/fn/math/signbit";
