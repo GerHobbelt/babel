aebbb9241e4de3f2be76e62d579d33f69f28edbe
Ger Hobbelt <ger@hobbelt.com>
2018-09-02 16:42:56 +0200

    regenerated library files produced these untracked files.   :-S     There's something spooky with the build processes (`make` vs. `make publish`)...

---
+++ packages/babel-preset-env/lib/available-plugins.js
@@ -0,0 +1,44 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _default = {
+  "syntax-async-generators": require("@gerhobbelt/babel-plugin-syntax-async-generators"),
+  "syntax-object-rest-spread": require("@gerhobbelt/babel-plugin-syntax-object-rest-spread"),
+  "syntax-optional-catch-binding": require("@gerhobbelt/babel-plugin-syntax-optional-catch-binding"),
+  "transform-async-to-generator": require("@gerhobbelt/babel-plugin-transform-async-to-generator"),
+  "proposal-async-generator-functions": require("@gerhobbelt/babel-plugin-proposal-async-generator-functions"),
+  "proposal-json-strings": require("@gerhobbelt/babel-plugin-proposal-json-strings"),
+  "transform-arrow-functions": require("@gerhobbelt/babel-plugin-transform-arrow-functions"),
+  "transform-block-scoped-functions": require("@gerhobbelt/babel-plugin-transform-block-scoped-functions"),
+  "transform-block-scoping": require("@gerhobbelt/babel-plugin-transform-block-scoping"),
+  "transform-classes": require("@gerhobbelt/babel-plugin-transform-classes"),
+  "transform-computed-properties": require("@gerhobbelt/babel-plugin-transform-computed-properties"),
+  "transform-destructuring": require("@gerhobbelt/babel-plugin-transform-destructuring"),
+  "transform-dotall-regex": require("@gerhobbelt/babel-plugin-transform-dotall-regex"),
+  "transform-duplicate-keys": require("@gerhobbelt/babel-plugin-transform-duplicate-keys"),
+  "transform-for-of": require("@gerhobbelt/babel-plugin-transform-for-of"),
+  "transform-function-name": require("@gerhobbelt/babel-plugin-transform-function-name"),
+  "transform-literals": require("@gerhobbelt/babel-plugin-transform-literals"),
+  "transform-modules-amd": require("@gerhobbelt/babel-plugin-transform-modules-amd"),
+  "transform-modules-commonjs": require("@gerhobbelt/babel-plugin-transform-modules-commonjs"),
+  "transform-modules-systemjs": require("@gerhobbelt/babel-plugin-transform-modules-systemjs"),
+  "transform-modules-umd": require("@gerhobbelt/babel-plugin-transform-modules-umd"),
+  "transform-object-super": require("@gerhobbelt/babel-plugin-transform-object-super"),
+  "transform-parameters": require("@gerhobbelt/babel-plugin-transform-parameters"),
+  "transform-shorthand-properties": require("@gerhobbelt/babel-plugin-transform-shorthand-properties"),
+  "transform-spread": require("@gerhobbelt/babel-plugin-transform-spread"),
+  "transform-sticky-regex": require("@gerhobbelt/babel-plugin-transform-sticky-regex"),
+  "transform-template-literals": require("@gerhobbelt/babel-plugin-transform-template-literals"),
+  "transform-typeof-symbol": require("@gerhobbelt/babel-plugin-transform-typeof-symbol"),
+  "transform-unicode-regex": require("@gerhobbelt/babel-plugin-transform-unicode-regex"),
+  "transform-exponentiation-operator": require("@gerhobbelt/babel-plugin-transform-exponentiation-operator"),
+  "transform-new-target": require("@gerhobbelt/babel-plugin-transform-new-target"),
+  "proposal-object-rest-spread": require("@gerhobbelt/babel-plugin-proposal-object-rest-spread"),
+  "proposal-optional-catch-binding": require("@gerhobbelt/babel-plugin-proposal-optional-catch-binding"),
+  "transform-regenerator": require("@gerhobbelt/babel-plugin-transform-regenerator"),
+  "proposal-unicode-property-regex": require("@gerhobbelt/babel-plugin-proposal-unicode-property-regex")
+};
+exports.default = _default;
